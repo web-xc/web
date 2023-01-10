66b9d2e6faa08c1812ea7942ba2f7217e062ece8
web-xc <68381226+web-xc@users.noreply.github.com>
2023-01-10 17:19:18 +0000

    优化css外链和压缩css/js Updated By Github Actions

--- js/mg.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("copy",(function(n){btf.snackbarShow("复制成功，请遵循GPL协议",!1,3e3)})),window.onkeydown=function(n){123===n.keyCode&&btf.snackbarShow("开发者模式已打开，请遵循GPL协议",!1,3e3)};
